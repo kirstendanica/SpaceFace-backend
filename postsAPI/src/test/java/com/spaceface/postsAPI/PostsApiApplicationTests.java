e86b9b44e3230526edaa5c6a02490c3af41a4f14
kirsten d <kirdanic@gmail.com>
2023-05-19 18:19:47 -0400

    Add generated Spring Boot project

---
+++ postsAPI/src/test/java/com/spaceface/postsAPI/PostsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spaceface.postsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
